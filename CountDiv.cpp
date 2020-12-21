d3c9a97f20a2b69fbc89fd0a45cac8b47249426b
kordian127 <kordian126@gmail.com>
2020-12-21 11:52:26 +0100

    Update CountDiv.cpp

--- CountDiv.cpp
+++ CountDiv.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
-
 using namespace std;
 
 int main(){
+int a=4;
+int b=11;
+int k=2;
+int p=0;
 
-
-
-
+for (int i=a;i<=b;i++){
+        if(i%k==0){
+            p+=1;
+    }
+}
+cout<<p<<endl;
 return 0;
 }
